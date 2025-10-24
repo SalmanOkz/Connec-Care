cffc651640a56beb7abd3577119092cb8ce3807d
SalmanOkz <muhammad.59195@iqra.edu.pk>
2025-10-24 19:48:58 +0500

    new changes

---
+++ next.config.ts
@@ -0,0 +1,29 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  basePath: process.env.BASEPATH,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/front-pages/landing-page',
+        permanent: true,
+        locale: false
+      },
+      {
+        source: '/dashboard',
+        destination: '/dashboards/crm',
+        permanent: true,
+        locale: false
+      },
+      {
+        source: '/((?!(?:en|front-pages|favicon.ico)\\b)):path',
+        destination: '/en/:path',
+        permanent: true,
+        locale: false
+      }
+    ]
+  }
+}
+
+export default nextConfig
---
+++ src/configs/primaryColorConfig.ts
@@ -0,0 +1,25 @@
+export type PrimaryColorConfig = {
+  name?: string
+  light?: string
+  main: string
+  dark?: string
+}
+
+// Primary color config object
+const primaryColorConfig: PrimaryColorConfig[] = [
+  
+  {
+    name: 'primary-2',
+    light: '#4EB0B1',
+    main: '#0D9394',
+    dark: '#096B6C'
+  },
+  {
+    name: 'primary-5',
+    light: '#5CAFF1',
+    main: '#2092EC',
+    dark: '#176BAC'
+  }
+]
+
+export default primaryColorConfig
